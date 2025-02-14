ead354ebdc143163b5111a72001f82ee167f51d2
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-02-14 14:49:07 +0530

    Complete create form for Startups

---
+++ startups/lib/actions.ts
@@ -0,0 +1,64 @@
+"use server";
+
+// Function for creatNewPitch
+import { auth } from "@/auth";
+import { parseServerActionResponse } from "@/lib/utils";
+import slugify from "slugify";
+import { writeClient } from "@/sanity/lib/write-client";
+
+export const createPitch = async (
+  state: any,
+  form: FormData,
+  pitch: string,
+) => {
+  const session = await auth();
+
+  // Check if the user already exists
+  if (!session) {
+    return parseServerActionResponse({
+      error: "Not signed in",
+      status: "ERROR",
+    });
+  }
+
+  // Destructure the request object
+  const { title, description, category, link } = Object.fromEntries(
+    Array.from(form).filter(([key]) => key !== "pitch"),
+  );
+
+  const slug = slugify(title as string, { lower: true, strict: true });
+
+  try {
+    const startup = {
+      title,
+      description,
+      category,
+      image: link,
+      slug: {
+        _type: slug,
+        current: slug,
+      },
+      author: {
+        _type: "reference",
+        _ref: session?.id,
+      },
+      pitch,
+    };
+
+    // Create the startup in Sanity and return the result
+    const result = await writeClient.create({ _type: "startup", ...startup });
+
+    return parseServerActionResponse({
+      ...result,
+      error: "",
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return parseServerActionResponse({
+      error: JSON.stringify(error),
+      status: "ERROR",
+    });
+  }
+};
